d3ad9060211ef2cffeedd6817a9d6c9b95d95f36
Niklas <mailaInte@a.b>
2015-01-14 12:54:04 +0100

    Main

--- main.cpp
+++ main.cpp
@@ -8,7 +8,14 @@ using namespace std;
 
 int main()
 {
-	cout << "HEJSAN"<< endl;
+    int m = 0;
+    int n = 0;
+	cout << "Choose m and n\nm: ";
+    cin >> m;
+    cout << "n: ";
+    cin >> n;
+    cout << "Creating a grid " << m << " x " << n << " points" << endl;
+
 
     Curve C(true);
     //Line L0(-10, 0, 5, 0);
@@ -18,7 +25,7 @@ int main()
 
     Domain D = Domain(C, L1, L2, L3);
     //Domain D = Domain(L0, L1, L2, L3);
-    D.generate_grid(30, 50);
+    D.generate_grid(m, n);
 
     D.writeToFile();
 
